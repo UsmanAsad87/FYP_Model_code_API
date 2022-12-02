783d46946bcdd84e1557c4add8af1711374369cb
ProgrammingForLife <usmanasad0234@gmail.com>
2022-12-02 15:28:54 +0500

    Multifacetest

--- api.py
+++ api.py
@@ -21,10 +21,16 @@ from deepface.commons import functions
 import pymongo
 import base64
 from datetime import datetime
+from mtcnn.mtcnn import MTCNN
+import cv2
 
 
 
 from flask import redirect,url_for ,render_template
+
+
+
+detector =MTCNN()
 
 #------------------------------
 
@@ -350,9 +356,27 @@ def searchByName(name):
 
 
 
+def loadBase64Img(uri):
+   encoded_data = uri.split(',')[1]
+   nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+   return img
 
+def extract_face(image, resize=(224, 224)):
+   faces = detector.detect_faces(image)
+   facesInb64=[]
+   for face in faces:
+    x1, y1, width, height = face['box']
+    x2, y2 = x1 + width, y1 + height
+    face_boundary = image[y1:y2, x1:x2]
+    face_image=cv2.resize(face_boundary,resize)
 
+    res, frame = cv2.imencode('.jpg', face_image)   
+    b64 = base64.b64encode(frame) 
+    img = "data:image/jpeg;base64," + b64.decode('utf-8')
+    facesInb64.append(img)
 
+   return facesInb64
 
 
 
@@ -394,7 +418,20 @@ def findfaceWrapper(req, trx_id = 0):
 #add for loop to iterate for searching the faces and call the below code on it
 	#-------------------------------------
 	#call represent function from the interface
+
+
+
+    
 	resultDf=pd.DataFrame()
+
+	#Just to check
+	img2=loadBase64Img(img)
+	face_imgs=extract_face(img2)
+	for i in face_imgs:
+		print(len(i))
+	#-----------------------
+
+
 	try:
 
 		resultDf = DeepFace.find(img
